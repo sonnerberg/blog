435c52e9e89691d0837ccacdccdf64b7b7cc35b6
Richard Nilsson <31686265+RichardNilsson@users.noreply.github.com>
2020-07-29 09:50:15 +0200

    Add support for react live

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,27 +1,28 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
 import React from "react";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
 import styled from "styled-components";
 import { copyToClipboard } from "../utils/copy-to-clipboard";
 
-export const Pre = styled.pre`
+const Pre = styled.pre`
+  position: relative;
   text-align: left;
-  margin: 1rem 0;
-  padding: 0.5rem;
+  margin: 1em 0;
+  padding: 0.5em;
   overflow-x: auto;
   border-radius: 3px;
 
-  & .token-line {
-    line-height: 1.3rem;
-    height: 1.3rem;
+  & .token-lline {
+    line-height: 1.3em;
+    height: 1.3em;
   }
   font-family: "Courier New", Courier, monospace;
-  position: relative;
 `;
 
-export const LineNo = styled.span`
+const LineNo = styled.span`
   display: inline-block;
-  width: 2rem;
+  width: 2em;
   user-select: none;
   opacity: 0.3;
 `;
@@ -38,7 +39,17 @@ const CopyCode = styled.button`
   }
 `;
 
-const Code = ({ codeString, language }) => {
+const Code = ({ codeString, language, ...props }) => {
+  if (props["react-live"]) {
+    return (
+      <LiveProvider code={codeString} noInline={true} theme={theme}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    );
+  }
+
   const handleClick = () => {
     copyToClipboard(codeString);
   };
